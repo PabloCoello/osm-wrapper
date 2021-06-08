fb1e098bad3b8488d05372d0a734fe5798dce857
Pablo Coello <pablo.coello@outlook.com>
2021-06-08 19:09:11 +0200

    Added basic functionality

---
+++ osm.py
@@ -0,0 +1,15 @@
+import requests
+import json
+overpass_url = "http://overpass-api.de/api/interpreter"
+overpass_query = """
+[out:json];
+area["ISO3166-1"="DE"][admin_level=2];
+(node["amenity"="biergarten"](area);
+ way["amenity"="biergarten"](area);
+ rel["amenity"="biergarten"](area);
+);
+out geom;
+"""
+response = requests.get(overpass_url, 
+                        params={'data': overpass_query})
+data = response.json()
